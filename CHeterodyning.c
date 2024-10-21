7c918ccb0cc71c8aa58cc9b8e1a09283c0b7c2ed
PhethaniMW <160791570+PhethaniMW@users.noreply.github.com>
2024-10-21 14:12:30 +0200

    Update CHeterodyning.c

--- CHeterodyning.c
+++ CHeterodyning.c
@@ -1,24 +1,28 @@
 #include "CHeterodyning.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h> // Include OpenMP for multi-threading
 
-extern float data [SAMPLE_COUNT];
+extern float data[SAMPLE_COUNT];
 extern float carrier[SAMPLE_COUNT];
+float result[SAMPLE_COUNT];
 
-float result [SAMPLE_COUNT];
-
-int main(int argc, char**argv){
-    printf("Running Unthreaded Test\n");
+int main(int argc, char** argv) {
+    printf("Running Optimized Test with Multi-Threading\n");
     printf("Precision sizeof %ld\n", sizeof(float));
-    
-
     printf("Total amount of samples: %ld\n", sizeof(data) / sizeof(data[0]));
-    
 
-    tic(); // start the timer
-    for (int i = 0;i<SAMPLE_COUNT;i++ ){
+    tic(); // Start the timer
+
+    // Parallelize the loop using OpenMP for multi-threading
+    #pragma omp parallel for
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
         result[i] = data[i] * carrier[i];
     }
-    double t = toc();
-    printf("Time: %lf ms\n",t/1e-3);
-    printf("End Unthreaded Test\n");
+
+    double t = toc(); // Stop the timer
+    printf("Time: %lf ms\n", t / 1e-3);
+    printf("End Optimized Test\n");
+
     return 0;
 }
